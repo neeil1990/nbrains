8819af3c4a183d22af50c40423ec2f4d7c35ac27
neeil1990 <neeil@mail.ru>
2019-05-13 14:30:38 +0300

    no message

--- bitrix/components/nbrains/main.feedback/templates/modal/lang/ru/template.php
+++ bitrix/components/nbrains/main.feedback/templates/modal/lang/ru/template.php
@@ -4,6 +4,6 @@ $MESS ['MFT_EMAIL'] = "Ваш E-mail";
 $MESS ['MFT_PHONE'] = "Телефон";
 $MESS ['MFT_MESSAGE'] = "Сообщение";
 $MESS ['MFT_CAPTCHA'] = "Защита от автоматических сообщений";
-$MESS ['MFT_SUBMIT_MODAL'] = "Заказать звонок";
+$MESS ['MFT_SUBMIT_MODAL'] = "Отправить";
 $MESS ['MFT_SUBMIT_CLOSE'] = "Закрыть";
 ?>
