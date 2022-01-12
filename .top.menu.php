285deb7105560d94c03fd9927a6175232f3685fd
neeil1990 <neeil@mail.ru>
2022-01-12 12:11:04 +0300

    menu

--- .top.menu.php
+++ .top.menu.php
@@ -8,29 +8,8 @@ $aMenuLinks = Array(
 		"" 
 	),
 	Array(
-		"Услуги", 
-		"/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Блог", 
-		"/blog/",
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
 		"Контакты", 
 		"/contact/", 
-		Array(), 
-		Array(), 
-		"" 
-	),
-	Array(
-		"Тест", 
-		"/test/", 
 		Array(), 
 		Array(), 
 		"" 
